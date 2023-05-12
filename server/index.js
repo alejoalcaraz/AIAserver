294cc4b448d2f7926492900ad0a36bc2786dcdcc
alejoalcaraz <a.alcaraz@uniandes.edu.co>
2023-05-12 01:49:59 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,15 +4,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3030;
 
-const wss = new WebSocket.Server({ port:process.env.PORT || 3030});
+const wss = new WebSocket.Server({ port: PORT });
 
 console.log("tiki");
 
-wss.on("connection", ws=>{
+wss.on("connection", ws => {
 
     console.log("New client connected");
 
     ws.on("close", () => {
         console.log("Client has disconnected");
     });
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
 });
